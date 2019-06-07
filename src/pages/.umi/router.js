a5e462fe30ce0d53d2531d3c0cd71d222db471b1
dzh1104 <zhding1104@163.com>
2019-06-08 00:38:15 +0800

    init

---
+++ src/pages/.umi/router.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import { Router as DefaultRouter, Route, Switch } from 'react-router-dom';
+import dynamic from 'umi/dynamic';
+import renderRoutes from 'umi/_renderRoutes';
+import history from '@tmp/history';
+
+
+const Router = DefaultRouter;
+
+const routes = [
+  {
+    "path": "/",
+    "component": require('../HelloWorld').default,
+    "exact": true
+  },
+  {
+    "component": () => React.createElement(require('/Users/jean/antd-course/node_modules/_umi-build-dev@1.9.2@umi-build-dev/lib/plugins/404/NotFound.js').default, { pagesPath: 'src/pages', hasRoutesInConfig: true })
+  }
+];
+window.g_routes = routes;
+const plugins = require('umi/_runtimePlugin');
+plugins.applyForEach('patchRoutes', { initialValue: routes });
+
+// route change handler
+function routeChangeHandler(location, action) {
+  plugins.applyForEach('onRouteChange', {
+    initialValue: {
+      routes,
+      location,
+      action,
+    },
+  });
+}
+history.listen(routeChangeHandler);
+routeChangeHandler(history.location);
+
+export { routes };
+
+export default function RouterWrapper() {
+  return (
+<Router history={history}>
+      { renderRoutes(routes, {}) }
+    </Router>
+  );
+}
