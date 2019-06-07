49221757f88a10d0c4e71da126fa7b73122e45d4
dzh1104 <zhding1104@163.com>
2019-06-08 03:35:49 +0800

    dva

--- src/pages/.umi/router.js
+++ src/pages/.umi/router.js
@@ -5,13 +5,55 @@ import renderRoutes from 'umi/_renderRoutes';
 import history from '@tmp/history';
 
 
-const Router = DefaultRouter;
+const Router = require('dva/router').routerRedux.ConnectedRouter;
 
 const routes = [
   {
     "path": "/",
-    "component": require('../HelloWorld').default,
-    "exact": true
+    "component": require('../../layout').default,
+    "routes": [
+      {
+        "path": "/",
+        "component": require('../Helloworld').default,
+        "exact": true
+      },
+      {
+        "path": "/helloworld",
+        "component": require('../Helloworld').default,
+        "exact": true
+      },
+      {
+        "path": "/puzzlecards",
+        "component": require('../puzzlecards').default,
+        "exact": true
+      },
+      {
+        "path": "/dashboard",
+        "routes": [
+          {
+            "path": "/dashboard/analysis",
+            "component": require('../Dashboard/Analysis').default,
+            "exact": true
+          },
+          {
+            "path": "/dashboard/monitor",
+            "component": require('../Dashboard/Monitor').default,
+            "exact": true
+          },
+          {
+            "path": "/dashboard/workplace",
+            "component": require('../Dashboard/Workplace').default,
+            "exact": true
+          },
+          {
+            "component": () => React.createElement(require('/Users/jean/antd-course/node_modules/_umi-build-dev@1.9.2@umi-build-dev/lib/plugins/404/NotFound.js').default, { pagesPath: 'src/pages', hasRoutesInConfig: true })
+          }
+        ]
+      },
+      {
+        "component": () => React.createElement(require('/Users/jean/antd-course/node_modules/_umi-build-dev@1.9.2@umi-build-dev/lib/plugins/404/NotFound.js').default, { pagesPath: 'src/pages', hasRoutesInConfig: true })
+      }
+    ]
   },
   {
     "component": () => React.createElement(require('/Users/jean/antd-course/node_modules/_umi-build-dev@1.9.2@umi-build-dev/lib/plugins/404/NotFound.js').default, { pagesPath: 'src/pages', hasRoutesInConfig: true })
